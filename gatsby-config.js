2386bd452ccb0f2dcf1e3822d25c282fdf1ce2a0
Simon Maher <simon.maher@integrify.io>
2019-08-04 21:42:07 +0300

    Added images, plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,17 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {}
+module.exports = {
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `src/posts`,
+      },
+    },
+    `gatsby-mdx`,
+  ],
+}
